aca0e82decfc4bccff3b7cb5ec8f8349a12fd7ab
Darren McMenamin <mcmenamin.darren@gmail.com>
2022-01-23 14:07:52 +0000

    Initial commit

---
+++ src/test/java/com/dmcme/boardgame/BoardgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmcme.boardgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
